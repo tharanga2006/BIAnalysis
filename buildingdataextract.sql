6b7aec1279e858b6f73bb2fa301202a50cece7e8
tharanga2006 <tharanga2006@gmail.com>
2017-07-12 15:25:24 +0530

    Add files via upload

---
+++ buildingdataextract.sql
@@ -0,0 +1,21 @@
+﻿
+CREATE SCHEMA tilemill;
+
+CREATE TABLE tilemill.buildings AS
+SELECT
+  p.identificatie::bigint, bouwjaar::int,
+  ST_Transform(p.geovlak, 4326) AS geom
+  
+FROM bagaveen.verblijfsobjectactueelbestaand v
+JOIN bagaveen.verblijfsobjectpandactueel vp
+  ON vp.identificatie = v.identificatie
+JOIN bagaveen.pandactueelbestaand p
+  ON vp.gerelateerdpand = p.identificatie
+JOIN bagaveen.verblijfsobjectgebruiksdoelactueel vg
+  ON v.identificatie = vg.identificatie
+GROUP BY
+  p.identificatie, bouwjaar, p.geovlak;
+
+CREATE INDEX buildings_geom_idx
+  ON tilemill.buildings
+  USING gist (geom);
